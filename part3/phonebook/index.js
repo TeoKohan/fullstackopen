6cdd73b28d1042b1e1b4cd9df36e65dcb946a1df
TeoKohan <teo.kohan@gmail.com>
2025-01-06 18:49:31 -0300

    Info page

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const now = new Date()
+
 const phonebook = [
     { 
       "id": "1",
@@ -30,6 +32,13 @@ app.get('/', (request, response) => {
   
 app.get('/api/persons', (request, response) => {
     response.json(phonebook)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${phonebook.length} people</p>
+         <p>${Date()}</p>`
+    )
 })
 
 const PORT = 3001
